cc7ebf6639193a1f5807aff3600089f401411532
Egor <boyko.e.r@gmail.com>
2020-02-29 22:34:03 +0300

    Подключение логгера

--- src/main/java/com/boyko/ifuture/log_finder/Main.java
+++ src/main/java/com/boyko/ifuture/log_finder/Main.java
@@ -1,9 +1,13 @@
 package com.boyko.ifuture.log_finder;
 
 import com.boyko.ifuture.log_finder.gui.MainWindow;
+import org.apache.log4j.Logger;
 
 public class Main {
+    private final static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("I started application");
         MainWindow app = new MainWindow();
         app.setVisible(true);
     }
