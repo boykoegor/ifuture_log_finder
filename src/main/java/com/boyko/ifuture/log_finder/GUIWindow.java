645122c6053c62599d0e7d9a910837b1840d8163
Egor <boyko.e.r@gmail.com>
2020-02-29 20:47:44 +0300

    Рефактор левой и правой панелей

--- src/main/java/com/boyko/ifuture/log_finder/GUIWindow.java
+++ src/main/java/com/boyko/ifuture/log_finder/GUIWindow.java
@@ -1,7 +1,5 @@
 package com.boyko.ifuture.log_finder;
 
-import sun.jvm.hotspot.ui.tree.SimpleTreeModel;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -10,99 +8,48 @@ import java.awt.event.ActionListener;
 
 public class GUIWindow extends JFrame {
 
-    private JLabel textLable = new JLabel("Текст:");
-    private JTextField text = new JTextField();
-    private JButton search = new JButton("Искать");
-    private JLabel wayLabel = new JLabel("Путь:");
-    private JButton way = new JButton("...");
-    private JLabel thisWay = new JLabel("Путь не указан");
-    private JLabel fileFormatLable = new JLabel("Формат файла");
-    private JTextField fileFormat = new JTextField(".log");
-    private JSplitPane panel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-    private JPanel leftPanel = new JPanel();
-    private JPanel rightPanel = new JPanel();
-    final JTabbedPane tabbedPane = new JTabbedPane();
-    private JPanel buttons = new JPanel();
+    private JLabel selectedPath = new JLabel("Путь не указан");
     private File selectedFile;
-    private JLabel countFiles = new JLabel("Текст обнаружен в данных файлах:");
-    private JTree treeModel = new JTree();
-    private JScrollPane scrollPane = new JScrollPane(treeModel,
-            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-
+    private static final Dimension WINDOW_SIZE = new Dimension(800, 600);
 
 
     GUIWindow() {
         super("Поисковик текста");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension dimension = toolkit.getScreenSize();
-        setBounds(dimension.width / 2 - 400, dimension.height / 2 - 300, 800, 600);
+        setWindowAtCenter();
 
-        leftPanel.setLayout(new BorderLayout());
-        leftPanel.setPreferredSize(new Dimension(250, 400));
-
-        leftPanel.add(countFiles, BorderLayout.NORTH);
-        leftPanel.add(scrollPane, BorderLayout.CENTER);
-
-        rightPanel.setLayout(new BorderLayout());
-        rightPanel.setPreferredSize(new Dimension(500, 400));
-
-        JButton add = new JButton("Добавить");
-        add.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tabbedPane.addTab("Вкладка ", new JPanel());
-            }
-        });
-        buttons.add(add);
-
-        JButton remove = new JButton("Удалить");
-        remove.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int select = tabbedPane.getSelectedIndex();
-                if (select >= 0) {
-                    tabbedPane.removeTabAt(select);
-                }
-            }
-        });
-        buttons.add(remove);
-
-        rightPanel.add(buttons, BorderLayout.SOUTH);
-        rightPanel.add(tabbedPane, BorderLayout.CENTER);
-
-        panel.setLeftComponent(leftPanel);
-        panel.setRightComponent(rightPanel);
+        JSplitPane panel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        panel.setLeftComponent(getTreePanel());
+        panel.setRightComponent(getPreviewTabbedPanel());
 
 
-        way.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser wayChooser = new JFileChooser();
-                wayChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int reply = wayChooser.showDialog(null, "Выбрать папку");
-                switch (reply) {
-                    case JFileChooser.APPROVE_OPTION:
-                        thisWay.setText(wayChooser.getSelectedFile().toString());
-                        selectedFile = wayChooser.getSelectedFile();
-                        break;
-                    case JFileChooser.CANCEL_OPTION:
-                        break;
-                    case JFileChooser.ERROR_OPTION:
-                        break;
-
-                }
+        JButton path = new JButton("...");
+        path.addActionListener(e -> {
+            JFileChooser wayChooser = new JFileChooser();
+            wayChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int reply = wayChooser.showDialog(null, "Выбрать папку");
+            switch (reply) {
+                case JFileChooser.APPROVE_OPTION:
+                    selectedPath.setText(wayChooser.getSelectedFile().toString());
+                    selectedFile = wayChooser.getSelectedFile();
+                    break;
+                case JFileChooser.CANCEL_OPTION:
+                    break;
+                case JFileChooser.ERROR_OPTION:
+                    break;
 
             }
+
         });
 
-        search.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (selectedFile != null) {
 
-                }
-
+        JButton search = new JButton("Искать");
+        search.addActionListener(e -> {
+            if (selectedFile != null) {
 
             }
+
+
         });
 
         GroupLayout layout = new GroupLayout(getContentPane());
@@ -110,36 +57,41 @@ public class GUIWindow extends JFrame {
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
 
+        JLabel fileFormatLabel = new JLabel("Формат файла");
+        JLabel textLabel = new JLabel("Текст:");
+        JTextField text = new JTextField();
+        JLabel pathLabel = new JLabel("Путь:");
+        JTextField fileFormat = new JTextField(".log");
         layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(textLable)
-                                .addComponent(wayLabel))
+                                .addComponent(textLabel)
+                                .addComponent(pathLabel))
                         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                 .addComponent(text)
                                 .addGroup(layout.createSequentialGroup()
-                                        .addComponent(way)
-                                        .addComponent(thisWay)
+                                        .addComponent(path)
+                                        .addComponent(selectedPath)
                                         .addGap(20)
-                                        .addComponent(fileFormatLable)
+                                        .addComponent(fileFormatLabel)
                                         .addComponent(fileFormat)))
                         .addComponent(search))
                 .addComponent(panel)
         );
 
-        layout.linkSize(SwingConstants.HORIZONTAL, search, fileFormat, way);
+        layout.linkSize(SwingConstants.HORIZONTAL, search, fileFormat, path);
 
         layout.setVerticalGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(textLable)
+                        .addComponent(textLabel)
                         .addComponent(text)
                         .addComponent(search))
                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(wayLabel)
-                        .addComponent(way)
-                        .addComponent(thisWay)
+                        .addComponent(pathLabel)
+                        .addComponent(path)
+                        .addComponent(selectedPath)
                         .addGap(20)
-                        .addComponent(fileFormatLable)
+                        .addComponent(fileFormatLabel)
                         .addComponent(fileFormat))
                 .addGroup(
                         layout.createParallelGroup(GroupLayout.Alignment.LEADING)
@@ -148,6 +100,52 @@ public class GUIWindow extends JFrame {
         );
 
 
+    }
+
+    private JPanel getPreviewTabbedPanel() {
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+        rightPanel.setPreferredSize(new Dimension(500, 400));
+
+        JButton add = new JButton("Добавить");
+        JTabbedPane tabbedPane = new JTabbedPane();
+        add.addActionListener(e -> tabbedPane.addTab("Вкладка ", new JPanel()));
+        JPanel buttons = new JPanel();
+        buttons.add(add);
+
+        JButton remove = new JButton("Удалить");
+        remove.addActionListener(e -> {
+            int select = tabbedPane.getSelectedIndex();
+            if (select >= 0) {
+                tabbedPane.removeTabAt(select);
+            }
+        });
+        buttons.add(remove);
+
+        rightPanel.add(buttons, BorderLayout.SOUTH);
+        rightPanel.add(tabbedPane, BorderLayout.CENTER);
+        return rightPanel;
+    }
+
+    private JPanel getTreePanel() {
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.setPreferredSize(new Dimension(250, 400));
+
+        JLabel countFiles = new JLabel("Текст обнаружен в данных файлах:");
+        leftPanel.add(countFiles, BorderLayout.NORTH);
+        JTree treeModel = new JTree();
+        JScrollPane scrollPane = new JScrollPane(treeModel,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        leftPanel.add(scrollPane, BorderLayout.CENTER);
+        return leftPanel;
+    }
+
+    private void setWindowAtCenter() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        setBounds(dimension.width / 2 - WINDOW_SIZE.width/2, dimension.height / 2 - WINDOW_SIZE.height/2, WINDOW_SIZE.width, WINDOW_SIZE.height);
     }
 
 }
