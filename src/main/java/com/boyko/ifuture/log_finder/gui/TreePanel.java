efbec3f6bea271180df67ddebbe154d7bce510fa
Egor <boyko.e.r@gmail.com>
2020-03-02 14:05:58 +0300

    add filter for files

--- src/main/java/com/boyko/ifuture/log_finder/gui/TreePanel.java
+++ src/main/java/com/boyko/ifuture/log_finder/gui/TreePanel.java
@@ -1,5 +1,7 @@
 package com.boyko.ifuture.log_finder.gui;
 
+import com.boyko.ifuture.log_finder.controller.FilesController;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,8 +12,9 @@ public class TreePanel extends JPanel {
         this.setPreferredSize(new Dimension(250, 400));
         JLabel countFiles = new JLabel("Текст обнаружен в данных файлах:");
         this.add(countFiles, BorderLayout.NORTH);
-        JTree treeModel = new JTree();
-        JScrollPane scrollPane = new JScrollPane(treeModel,
+        JTree tree = new JTree(FilesController.getTreeFiles("/Users/boyko.e.r/IdeaProjects/ifuture_log_finder/src/main"));
+        tree.setRootVisible(false);
+        JScrollPane scrollPane = new JScrollPane(tree,
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         this.add(scrollPane, BorderLayout.CENTER);
